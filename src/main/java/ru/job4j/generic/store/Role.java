34a012ae317411fe9efa674721a61932e2cd1168
Vladimir Kudriavtsev <ztmwtm@gmail.com>
2023-03-31 01:45:03 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/main/java/ru/job4j/generic/store/Role.java
@@ -0,0 +1,15 @@
+package ru.job4j.generic.store;
+
+public class Role extends Base {
+
+    private final String role;
+
+    public Role(String id, String role) {
+        super(id);
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
