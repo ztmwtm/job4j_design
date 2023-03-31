7bd17b4d35275bbb69fe99ceb53bf6959dbb53a6
Vladimir Kudriavtsev <ztmwtm@gmail.com>
2023-03-31 23:51:58 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/simplelist/SimpleArrayList.java
@@ -0,0 +1,86 @@
+package ru.job4j.collection.simplelist;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            container = grow(container);
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        System.arraycopy(container, Integer.min(index + 1, size - 1), container, index, size - 1 - index);
+        container[size - 1] = null;
+        size--;
+        modCount++;
+        return oldValue;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            final int expectedModCount = modCount;
+            int point = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[point++];
+            }
+        };
+    }
+
+    private T[] grow(T[] container) {
+        T[] newContainer = (T[]) new Object[Integer.max(container.length, 1) << 1];
+        System.arraycopy(container, 0, newContainer, 0, container.length);
+        return newContainer;
+    }
+}
