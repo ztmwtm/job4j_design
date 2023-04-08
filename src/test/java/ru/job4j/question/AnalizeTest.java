971c139a36b1addb4b846e542e7618d13db9ba52
Vladimir Kudriavtsev <ztmwtm@gmail.com>
2023-04-08 16:23:02 +0300

    3. Статистика по коллекции. [#45889]

--- src/test/java/ru/job4j/question/AnalizeTest.java
+++ src/test/java/ru/job4j/question/AnalizeTest.java
@@ -1,8 +1,11 @@
 package ru.job4j.question;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
 import java.util.Set;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class AnalizeTest {
 
@@ -53,6 +56,43 @@ class AnalizeTest {
         User u3 = new User(3, "C");
         Set<User> previous = Set.of(u1, u2, u3);
         Set<User> current = Set.of(new User(1, "AA"), u2, new User(4, "D"));
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(1, 1, 1));
+    }
+    
+    @Test
+    void whenAllDeleted() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Collections.emptySet();
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 0, 3));
+    }
+
+    @Test
+    void whenAllAdded() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set<User> previous = Collections.emptySet();
+        Set<User> current = Set.of(u1, u2, u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(3, 0, 0));
+    }
+
+    @Test
+    void whenEmpty() {
+        Set<User> previous = Collections.emptySet();
+        Set<User> current = Collections.emptySet();
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 0, 0));
+    }
+
+    @Test
+    void whenAddedAndDeletedAndChanged() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Set.of(u1, new User(2, "BB"), new User(4, "CC"));
         assertThat(Analize.diff(previous, current)).isEqualTo(new Info(1, 1, 1));
     }
 }
