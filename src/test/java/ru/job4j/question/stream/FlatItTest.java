0484676d0eb116ceb3c1cee3cb0efdeb7480edd9
Vladimir Kudriavtsev <ztmwtm@gmail.com>
2023-04-16 20:03:59 +0300

    2.1. FileInputStream [#4898]

--- src/test/java/ru/job4j/question/stream/FlatItTest.java
+++ src/test/java/ru/job4j/question/stream/FlatItTest.java
@@ -10,12 +10,8 @@ import static org.assertj.core.api.Assertions.*;
 class FlatItTest {
     @Test
     void whenIt() {
-        Iterator<Iterator<Integer>> it = List.of(
-                List.of(1).iterator(),
-                List.of(2, 3).iterator()
-        ).iterator();
-        assertThat(
-                FlatIt.flatten(it)).isEqualTo
-                (List.of(1, 2, 3));
+        Iterator<Iterator<Integer>> it = List.of(List.of(1).iterator(), List.of(2, 3).iterator()).iterator();
+        assertThat(FlatIt.flatten(it))
+                .isEqualTo(List.of(1, 2, 3));
     }
 }
