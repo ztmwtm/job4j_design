ab6e392b045f2bf5e38b51ee8eae9242bc75c690
Vladimir Kudriavtsev <ztmwtm@gmail.com>
2023-04-17 16:53:37 +0300

    CheckStyle fixes

--- src/main/java/ru/job4j/question/stream/FlatIt.java
+++ src/main/java/ru/job4j/question/stream/FlatIt.java
@@ -18,4 +18,17 @@ public class FlatIt {
     private static <T> Stream<T> iteratorToStream(Iterator<T> it) {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED), false);
     }
+
+    public static void showArray(Integer[] data) {
+        Stream.iterate(0, i -> i < data.length, i -> i + 2)
+                .forEach(i -> System.err.println(data[i]));
+    }
+
+    public static void main(String[] args) {
+        showArray(new Integer[]{1, 2, 3, 4, 5, 6});
+    }
+
+    public static Stream<Object> createStream(Integer[] data) {
+        return Stream.builder().add(data[0]).add(data[data.length - 1]).build();
+    }
 }
